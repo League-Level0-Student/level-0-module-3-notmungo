d8116504ab21744cf95b7bd5635dbc96bcdb457e
league <league@imac-24.attlocal.net>
2022-09-24 17:58:26 -0700

    Finished everything in the staged changes didnt start anything else.

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -6,20 +6,48 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.awt.geom.Ellipse2D;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
 		Robot robot = new Robot("batman");
-	    //2. Make the robot draw a shape (this will take more than one line of code)	
-		    
+	    //2. Make the robot draw a shape (this will take more than one line of code
+		robot.penDown();
+		while(true) {
+		robot.move(80);
+		robot.turn(90);
+		robot.move(80);
+		robot.turn(90);
+		robot.move(80);
+		robot.turn(90);
+		robot.move(80);
+		robot.setSpeed(10);
+		
 		//3. Set the pen width to 10
+		robot.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
+		String penColor = JOptionPane.showInputDialog( "What color pen would you like the robot to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if (penColor .equals("red")) {
+			robot.setPenColor(Color.red);
+		}
+		else if(penColor.equals("blue")) {
+			robot.setPenColor(Color.blue);
+		}
+		else if(penColor.equals("black")) {
+			robot.setPenColor(Color.black);
+		}
+		else if (penColor.equals("white")) {
+			robot.setPenColor(Color.white);
+		}
+		else {
+			robot.setRandomPenColor();
+		}
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+		}
 
 		
 
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -3,26 +3,47 @@ package _01_else_if._3_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100)+1;
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+		for (int i = 0; i<10; i ++) {
+			
+		
+		String guess = JOptionPane.showInputDialog( "Give me a guess");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			
+			int guess1 = Integer.parseInt(guess);
+		
 			// 5. if the guess is correct
+			if (guess1 == random) {
+				JOptionPane.showMessageDialog(null, "Win");
+				System.exit(0);
+			}
+			else if (guess1 > random){
+				JOptionPane.showMessageDialog(null, "Too high");
+				JOptionPane.showMessageDialog(null, "You lost!");
+			}
+			else if (guess1 < random) {
+				JOptionPane.showMessageDialog(null, "Too low");
+				JOptionPane.showMessageDialog(null, "You lost!");
+			}
+			}
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
 				// 8. Tell them it's too high
 			// 9. if the guess is low
+			
 				// 10. Tell them it's too low
 
 		// 13. Tell them they lose
